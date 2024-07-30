149b563173d4d47a2f641d38d39e56c692f5a536
Pardeep Shyoran <143327162+pardeep75889@users.noreply.github.com>
2024-07-30 13:48:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/Day%2025/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -59,7 +59,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (track, pause = "false") => {
-    currentSong.src = `/Day 25/${currFolder}/` + track
+    currentSong.src = `/${currFolder}/` + track
     if (!pause) {
         currentSong.play();
         play.src = "img/pause.svg"
@@ -70,7 +70,7 @@ const playMusic = (track, pause = "false") => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`/Day%2025/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -82,13 +82,13 @@ async function displayAlbums() {
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[0]
 
-            let a = await fetch(`/Day%2025/songs/${folder}/info.json`);
+            let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
                         <div class="play">
                             <i class="fa-solid fa-circle-play"></i>
                         </div>
-                        <img src= "/Day%2025/songs/${folder}/cover.jpeg" alt="Playlists Image">
+                        <img src= "/songs/${folder}/cover.jpeg" alt="Playlists Image">
                         <h2>${response.title}</h2>
                         <p>${response.description}</p>
                     </div>`
